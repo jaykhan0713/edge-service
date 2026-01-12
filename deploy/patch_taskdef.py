0af614bf68d318ba65660d33dfd79980b747f107
Jay <jayykhan0713@gmail.com>
2026-01-12 08:43:26 -0800

    updated buildspec.yml

--- deploy/patch_taskdef.py
+++ deploy/patch_taskdef.py
@@ -3,12 +3,26 @@ import os
 
 IMAGE_URI = os.environ["IMAGE_URI"]
 
-with open("deploy/taskdef.base.json", "r") as f:
+with open("taskdef.json", "r") as f:
     td = json.load(f)
 
 for c in td.get("containerDefinitions", []):
     if c.get("name") == "app":
         c["image"] = IMAGE_URI
+
+readonly = [
+    "taskDefinitionArn",
+    "revision",
+    "status",
+    "requiresAttributes",
+    "compatibilities",
+    "registeredAt",
+    "registeredBy",
+    "deregisteredAt",
+]
+
+for k in readonly:
+    td.pop(k, None)
 
 with open("taskdef.json", "w") as f:
     json.dump(td, f, indent=2)
